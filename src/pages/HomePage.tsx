24b47c09209d354f1f2fe61dc8345195090b8078
ph-1dnjs <ph.1dnjs@gmail.com>
2025-05-03 17:28:19 +0900

    feat: tailwindcss 및 절대 경로 적용 #2

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray2'>
+      <h1 className='text-3xl font-bold'>Rolling Paper</h1>
+    </div>
+  );
+};
+
+export default HomePage;
