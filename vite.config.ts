9cb1d7c88bbdb132bfa1892ec26d54f29ecb89d2
luwbe1 <luwbe1@naver.com>
2025-05-09 01:07:42 +0900

    Feature: prettier + svg 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
